de0d4a3bcebd742791fba3c20d4792569a851efb
Joao Bueno <joaobueno121212@gmail.com>
2025-02-03 14:10:33 -0300

    component BookOverview finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,16 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
 
 const Home = () => (
   <div className="">
-    <BookList/>
-    <BookOverview/>
+    <BookOverview {...sampleBooks[0]}/>
+
+    <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+    />
   </div>
 );
 
--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,7 +1,11 @@
+import { Smooch_Sans } from "next/font/google";
+
+const smooch_Sans = Smooch_Sans({ subsets: ['latin'] });
+
 export default function Page() {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <h2 className={`${smooch_Sans.className} antialiased font-bebas-neue text-4xl text-light-100`}>Popular Books</h2>
+        </section>
     );
 }
