a08b2ff55ddca74e7c602ce20704ff5cc73dd526
Joao Bueno <joaobueno121212@gmail.com>
2025-02-03 15:08:28 -0300

    finishing bookcard

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,11 +1,26 @@
 import { Smooch_Sans } from "next/font/google";
+import BookCard from "./BookCard";
 
 const smooch_Sans = Smooch_Sans({ subsets: ['latin'] });
 
-export default function Page() {
+interface Props {
+    title: string;
+    books: Book[];
+    containerClassName?: string;
+}
+
+export default function BookList({ title, books, containerClassName }: Props) {
+    if (books.length < 2) return;
+
     return (
-        <section>
-            <h2 className={`${smooch_Sans.className} antialiased font-bebas-neue text-4xl text-light-100`}>Popular Books</h2>
+        <section className={containerClassName}>
+            <h2 className={`${smooch_Sans.className} antialiased font-bebas-neue text-4xl text-light-100`}>{title}</h2>
+
+            <ul className="book-list">
+                {books.map((book) => (
+                    <BookCard key={book.title} {...book} />
+                ))}
+            </ul>
         </section>
     );
 }
